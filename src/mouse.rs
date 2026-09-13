fdddfadc33400e3a52e45001173d5b2c7010e7f0
SimonDevStack <254177992+SimonDevStack@user.noreply.github.com>
2026-09-13 22:13:49 +0200

    Added some basic (not working yet) parsing

--- src/mouse.rs
+++ src/mouse.rs
@@ -1,5 +1,6 @@
 use std::process::Command;
-
+use std::str::from_utf8;
+use std::str::split_once;
 pub fn location() -> Result<(i32, i32), Error> {
     match recognize_compositor() {
         "hyprland" => hyprland_location()?,
@@ -12,5 +13,12 @@ fn recognize_compositor() -> &str {
 }
 
 fn hyprland_location() -> Result<(i32, i32), Error> {
-    Command::new("hyprctl").arg("cursorpos").output?;
+    let result = Command::new("hyprctl").arg("cursorpos").output();
+}
+
+fn parse(data: Vec<u8>) {
+    // convert bytes to slices
+    let data = from_utf8(&data[0..]);
+    let (x, y) = data.split_once(',').unwrap();
+    let x = &x[1..];
 }
